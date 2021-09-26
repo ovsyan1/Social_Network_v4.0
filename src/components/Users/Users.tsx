d320d4f5c9ede983d8812586cc45ccbb5be75422
Kyrylo Ovsiannik <ovsyasha16@gmail.com>
2021-09-26 17:38:21 +0300

    useHistory

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,10 +5,14 @@ import UsersSearchForm from './UsersSearchForm';
 import { FilterType, requestUsers } from '../../redux/users_reducer';
 import { useDispatch, useSelector } from 'react-redux';
 import { getCurrentPage, getPageSize, getTotalItemsCount, getUsersFilter, getUsers, getFollowingInProgress } from '../../redux/users_selectors';
+import { useHistory } from 'react-router-dom'; // ? react-router
+import queryString from 'querystring'
 
 type PropsType = {}
 
-export const Users: React.FC<PropsType> = (props) => {
+type QueryParamsType = {term?: string, page?: string, friend?: string}
+
+export const Users: React.FC<PropsType> = React.memo((props) => {
 
         const users = useSelector(getUsers)
         const totalItemsCount = useSelector(getTotalItemsCount) // same with mapStateToProps
@@ -18,10 +22,33 @@ export const Users: React.FC<PropsType> = (props) => {
         const followingInProgress = useSelector(getFollowingInProgress)
 
         const dispatch = useDispatch()
+        const history = useHistory()
 
         useEffect(() => {
-            dispatch(requestUsers(currentPage, pageSize, filter));
+            const parsed = queryString.parse(history.location.search.substr(1)) as QueryParamsType
+
+            let actualPage = currentPage
+            let actualFilter = filter
+
+            if(parsed.page) actualPage = +parsed.page
+            if(parsed.term) actualFilter = {...actualFilter, term: parsed.term as string}
+            if(parsed.friend) actualFilter = {...actualFilter, friend: parsed.friend === "null" ? null : parsed.friend === "true" ? true : false}
+
+            dispatch(requestUsers(actualPage, pageSize, actualFilter));
         }, [])
+
+        useEffect(() => {
+            const query: QueryParamsType = {}
+            if(filter.term) query.term = filter.term
+            if(filter.friend !== null) query.friend = String(filter.friend)
+            if(currentPage !== 1) query.page = String(currentPage)
+
+            history.push({
+                pathname: '/users',
+                // search: `?term=${filter.term}&friend=${filter.friend}&page=${currentPage}`
+                search: queryString.stringify(query)
+            })
+        }, [filter, currentPage, history])
 
         const onPageChanged = (pageNumber: number) => {
             dispatch(requestUsers(pageNumber, pageSize, filter))
@@ -58,4 +85,4 @@ export const Users: React.FC<PropsType> = (props) => {
                 })}
         </div>
     )
-}
+})
--- src/components/Users/UsersSearchForm.tsx
+++ src/components/Users/UsersSearchForm.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import {Formik, Form, Field} from 'formik';
 import {FilterType} from '../../redux/users_reducer';
+import { useSelector } from 'react-redux';
+import { getUsersFilter } from '../../redux/users_selectors';
 
 const usersSearchFormValidate = (values: any) => {
     const errors = {};
     return errors;
 }
 
+type FriendFormType = "true" | "false" | "null"
+
 type FormType = {
     term: string
-    friend: "true" | "false" | "null"
+    friend: FriendFormType
 }
 
 type PropsType = {
@@ -17,21 +21,23 @@ type PropsType = {
 }
 
 const UsersSearchForm: React.FC<PropsType> = React.memo((props) => {
+    const filter = useSelector(getUsersFilter)
 
-const submit = (values: FormType, { setSubmitting }: {setSubmitting: (isSubmitting: boolean) => void}) => {
-    const filter: FilterType = {
-        term: values.term,
-        friend: values.friend === "null" ? null : values.friend === "true" ? true : false
+    const submit = (values: FormType, { setSubmitting }: {setSubmitting: (isSubmitting: boolean) => void}) => {
+        const filter: FilterType = {
+            term: values.term,
+            friend: values.friend === "null" ? null : values.friend === "true" ? true : false
+        }
+        
+        props.onFilterChanged(filter)
+        setSubmitting(false)
     }
-    
-    props.onFilterChanged(filter)
-    setSubmitting(false)
-}
 
 return (
     <div>
         <Formik
-            initialValues={{ term: '', friend: 'null' }}
+            enableReinitialize
+            initialValues={{ term: filter.term, friend: String(filter.friend) as FriendFormType }}
             validate={usersSearchFormValidate}
             onSubmit={submit}>
         {({ isSubmitting }) => (
